af0145698fc3be6cce76dad695a06a386916e0ef
duyluc <duongduyluc3@gmail.com>
2022-04-20 16:07:16 +0700

    Update Changing!

--- ADXL345.py
+++ ADXL345.py
@@ -299,8 +299,8 @@ class ADXL345:
     def RollPitch(self):
         try:
             (accel_x, accel_y, accel_z) = self.getAxes()
+            roll = math.atan(accel_y/math.sqrt(math.pow(accel_x,2) + math.pow(accel_z,2)))* 180/math.pi
+            pitch = math.atan(-1 * accel_x/math.sqrt(math.pow(accel_y,2) + math.pow(math.pow(accel_z,2)))) *180/math.pi
         except Exception as e:
             print(str(e))
-        roll = math.atan(accel_y/math.sqrt(math.pow(accel_x,2) + math.pow(accel_z,2)))* 180/math.pi
-        pitch = math.atan(-1 * accel_x/math.sqrt(math.pow(accel_y,2) + math.pow(math.pow(accel_z,2)))) *180/math.pi
         return (roll, pitch)
