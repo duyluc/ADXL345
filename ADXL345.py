962e894f5cbeaf7f6131c9fdd75ea76462c7ea38
duyluc <duongduyluc3@gmail.com>
2022-04-20 15:45:03 +0700

    Update Changing!

--- ADXL345.py
+++ ADXL345.py
@@ -288,4 +288,5 @@ class ADXL345:
         (accel_x, accel_y, accel_z) = self.getAxes()
         roll = math.atan(accel_y/math.sqrt(math.pow(accel_x,2) + math.pow(accel_z,2)))* 180/math.pi
         pitch = math.atan(-1 * accel_x/math.sqrt(math.pow(accel_y,2) + math.pow(math.pow(accel_z,2)))) *180/math.pi
+        print(str("%d --- %d"%roll%pitch))
         return (roll, pitch)
